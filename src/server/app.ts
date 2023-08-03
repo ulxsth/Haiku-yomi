90ce9fad15e9daff8b14e48b8db4401a1efb8f8b
yotu <114195789+ulxsth@users.noreply.github.com>
2023-08-04 08:06:32 +0900

    init: Express.jsの起動ファイル作成

---
+++ src/server/app.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import path from 'path'
+
+const port = 8080
+const app = express()
+app.use(express.static('dist'))
+
+app.get(/.*/, (req: express.Request, res: express.Response) => {
+  res.sendFile(path.resolve('dist/index.html'))
+})
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
