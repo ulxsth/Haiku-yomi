da445e8bdceeaaa784c8da7b4e55bf35c687d74d
yotu <114195789+ulxsth@users.noreply.github.com>
2023-08-04 08:12:36 +0900

    init: express用に設定を追加

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+module.exports = {
+  devServer: {
+    port: 8081,
+    // localhostでvueからexpressにAPIリクエストを送信する為の設定
+    proxy: 'http://localhost:8080'
+  },
+  configureWebpack: {
+    devtool: 'source-map'
+  }
+}
