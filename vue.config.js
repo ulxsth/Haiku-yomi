1ef70024a3283283392a3a0b7a019976e2e2144d
yotu <114195789+ulxsth@users.noreply.github.com>
2023-08-04 10:27:40 +0900

    refactor

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ module.exports = {
   devServer: {
     port: 8081,
     // localhostでvueからexpressにAPIリクエストを送信する為の設定
-    proxy: 'http://localhost:8080'
+    proxy: "http://localhost:8080",
   },
   configureWebpack: {
-    devtool: 'source-map'
-  }
-}
+    devtool: "source-map",
+  },
+};
