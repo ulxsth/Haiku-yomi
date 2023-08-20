d27671e38fd124f3ea20f239a951fa4f800d30bd
Gingira1213 <gin12132828@icloud.com>
2023-08-20 14:47:10 +0900

    初コミット
    signin画面とマッチング中画面とホーム画面の外枠だけ

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="app" class="app-container">
+    <SignIn msg="" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import SignIn from "./components/SignIn.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    SignIn,
   },
 })
 export default class App extends Vue {}
@@ -22,8 +21,20 @@ export default class App extends Vue {}
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #0b0b0b;
+  overflow: hidden; /* 画像のオーバーフローを制御 */
+}
+
+.app-container {
+  background-image: url(~@/assets/background.png); /* require() を使用 */
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  background-position: center center; /* 画像を中央に配置 */
+  width: 100vw; /* 画面幅に合わせる */
+  height: 100vh; /* 画面高さに合わせる */
+  position: absolute; /* ページ全体を覆うように */
+  top: 0;
+  left: 0;
+  z-index: -1; /* 背面に配置 */
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="HomePage-container">
+    <button class="button-css" @click="submit">
+      <div class="title">俳諧の間<br /></div>
+      <div class="subtitle">ランダムなプレイヤーと俳句を競います</div>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class HomePage extends Vue {
+  // ここに必要な処理を追加することができます
+}
+</script>
+
+<style scoped>
+.HomePage-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Mincho", sans-serif;
+  margin: 0 auto; /* マージンを調整することで中央に配置 */
+  padding: 2rem;
+  max-width: 1000px;
+  height: 100vh; /* 画面全体の高さに合わせる */
+}
+
+.button-css {
+  margin-top: 5.5rem;
+  background: #89aca0;
+  border: none;
+  font-size: 2.4rem;
+  margin-top: 3.5rem;
+  margin-bottom: 2rem;
+  border-radius: 5px;
+  padding: 1rem 4rem;
+  text-align: center;
+  box-shadow: 2.5px 2.5px #336140;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+}
+
+@font-face {
+  font-family: "Mincho"; /* フォント名を指定 */
+  src: url(~@/assets/fonts/SawarabiMincho-Regular.ttf); /* フォントファイルへのパスを指定 */
+}
+
+/* titleのグラデーション */
+.title {
+  font-family: "Mincho", sans-serif;
+  font-size: 4.5rem;
+  margin-top: 10px;
+  margin: 10px 0 0;
+  text-align: center;
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  background-image: linear-gradient(
+    to bottom,
+    rgb(0, 0, 0),
+    rgba(106, 106, 106, 0)
+  );
+}
+
+.subtitle {
+  font-size: 1.3rem;
+}
+</style>
